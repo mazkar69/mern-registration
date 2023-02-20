c9b97075ed12dd29d441e64d3a990307729ce538
Mohd Azkar <mohdazkar@outlook.com>
2023-02-20 16:14:15 +0530

    production changes

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express();
 const cookieParser = require('cookie-parser')
+const dotenv = require('dotenv').config()
 require("./db/conn")
 const port = process.env.PORT || 5000;
 
@@ -16,7 +17,9 @@ app.use(express.json());
 app.use(require('./router/auth'))
 
 
-if ( process.env.NODE_ENV == "production"){
+
+//uncomment from .env to run this if production
+if (process.env.NODE_ENV == "production") {
 
     app.use(express.static("client/build"));
 
@@ -24,14 +27,19 @@ if ( process.env.NODE_ENV == "production"){
 
     app.get("*", (req, res) => {
 
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 
     })
 
 
 }
+else {
+    app.get('/', (req, res) => {
+        res.send("Running..........")
+    })
+}
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log("Server is running at port ", port);
 })
